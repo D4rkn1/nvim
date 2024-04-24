4e8842c9154aa90e2b3c059bdce72858849a9879
D4rkn1 <no@email.com>
2024-04-24 08:04:07 +0000

    add cmp and luasnip

---
+++ lua/khaos/lazy/cmp.lua
@@ -0,0 +1,49 @@
+return {
+	'hrsh7th/nvim-cmp',
+	dependencies = {
+		'hrsh7th/cmp-nvim-lsp',
+		'hrsh7th/cmp-buffer',
+		'hrsh7th/cmp-path',
+		'hrsh7th/cmp-cmdline',
+		'L3MON4D3/LuaSnip',
+		'saadparwaiz1/cmp_luasnip',
+	},
+
+	config = function()
+
+		local cmp = require('cmp')
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					 require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+				end,
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			mapping = cmp.mapping.preset.insert({
+				['<C-b>'] = cmp.mapping.scroll_docs(-4),
+				['<C-f>'] = cmp.mapping.scroll_docs(4),
+				['<C-y'] = cmp.mapping.complete(),
+				['<C-e>'] = cmp.mapping.abort(),
+				['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+			}),
+			sources = cmp.config.sources({
+				{ name = 'nvim_lsp' },
+				{ name = 'luasnip' }, -- For luasnip users.
+			}, {
+				{ name = 'buffer' },
+			})
+		})
+
+		-- Set up lspconfig.
+		local capabilities = require('cmp_nvim_lsp').default_capabilities()
+		-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+		require('lspconfig')['lua_ls'].setup {
+			capabilities = capabilities
+		}
+
+	end,
+}
