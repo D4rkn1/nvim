b281765eb08bffeeb7b6d81929f4f93c7e451fa0
D4rkn1 <no@email.com>
2024-04-24 11:35:27 +0000

    add yank to primary clipboard

--- lua/khaos/remap.lua
+++ lua/khaos/remap.lua
@@ -2,3 +2,6 @@ vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
 vim.keymap.set("n", "<leader>s", "<cmd>bd<CR>")
 vim.keymap.set("n", "<leader>q", "<cmd>qa!<CR>")
 vim.keymap.set("n", "<leader>L", "<cmd>:Lazy<CR>")
+vim.keymap.set("v", "<leader>y", [["*y]])
+
+
