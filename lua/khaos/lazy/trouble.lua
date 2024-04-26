22a4e69be3906d8259e7a2837924b0385c11c27b
D4rkn1 <no@email.com>
2024-04-26 12:39:04 +0000

    more trouble fix

--- lua/khaos/lazy/trouble.lua
+++ lua/khaos/lazy/trouble.lua
@@ -5,16 +5,14 @@ return {
 	},
 	config = function()
 		local trouble = require("trouble")
-		vim.keymap.set("n", "<leader>d<leader>", function() trouble.toggle() end)
+		vim.keymap.set("n", "<leader>d<leader>", function() trouble.toggle("workspace_diagnostics") end)
 		vim.keymap.set("n", "<leader>dj", function()
-			if not trouble.is_open() then trouble.toggle() end
+			if not trouble.is_open() then trouble.toggle("workspace_diagnostics") end
 			require("trouble").next({skip_groups = true , jump = true})
-			if trouble.is_open() then trouble.toggle() end
 		end)
 		vim.keymap.set("n", "<leader>dk", function()
-			if not trouble.is_open() then trouble.toggle() end
+			if not trouble.is_open() then trouble.toggle("workspace_diagnostics") end
 			require("trouble").previous({skip_groups = true , jump = true})
-			if trouble.is_open() then trouble.toggle() end
 		end)
 	end,
 }
