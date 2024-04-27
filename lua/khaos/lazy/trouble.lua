58aaeda411d637abf07f8c5106dd20515e1cf365
D4rkn1 <no@email.com>
2024-04-27 13:36:16 +0000

    trouble and lsp reconfig

--- lua/khaos/lazy/trouble.lua
+++ lua/khaos/lazy/trouble.lua
@@ -8,11 +8,11 @@ return {
 		vim.keymap.set("n", "<leader>d<leader>", function() trouble.toggle("workspace_diagnostics") end)
 		vim.keymap.set("n", "<leader>dj", function()
 			if not trouble.is_open() then trouble.toggle("workspace_diagnostics") end
-			require("trouble").next({skip_groups = true , jump = true})
+			require("trouble").next({ skip_groups = true, jump = true })
 		end)
 		vim.keymap.set("n", "<leader>dk", function()
 			if not trouble.is_open() then trouble.toggle("workspace_diagnostics") end
-			require("trouble").previous({skip_groups = true , jump = true})
+			require("trouble").previous({ skip_groups = true, jump = true })
 		end)
 	end,
 }
