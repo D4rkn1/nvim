8037ac65de4c4d6895759cd19d53275cd9cf3705
D4rkn1 <no@email.com>
2024-04-27 15:43:28 +0000

    reconfig incline

--- lua/khaos/lazy/incline.lua
+++ lua/khaos/lazy/incline.lua
@@ -1,7 +1,20 @@
 return {
 	"b0o/incline.nvim",
 	config = function()
-		require("incline").setup()
+		require("incline").setup {
+			render = function(props)
+				local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+				local modified = vim.bo[props.buf].modified
+				local normalColor = '#eeeeee'
+				local modifiedColor = '#e84713'
+				local color = modified and modifiedColor or normalColor
+				return {
+					filename,
+					modified and { ' *' } or '',
+					guifg = color,
+				}
+			end
+		}
 	end,
 	event = 'VeryLazy',
 }
