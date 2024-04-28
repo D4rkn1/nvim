9c3b6146789c3948084c41cf381dce182ed8e5e5
D4rkn1 <no@email.com>
2024-04-28 11:27:21 +0000

    oil: no prompt on simple edit

--- lua/khaos/lazy/oil.lua
+++ lua/khaos/lazy/oil.lua
@@ -1,7 +1,12 @@
 return {
 	"stevearc/oil.nvim",
 	config = function()
-		require("oil").setup()
+		require("oil").setup({
+			columns = {
+				"icon",
+			},
+			skip_confirm_for_simple_edits = true,
+		})
 		vim.keymap.set("n", "<leader>,", "<CMD>Oil<CR>")
 	end,
 }
