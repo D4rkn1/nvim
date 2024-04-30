9499940ada8ab807a220a98b4aa0acbe9aaf0e74
D4rkn1 <no@email.com>
2024-04-30 14:53:47 +0000

    add close window shortcut

--- lua/khaos/remap.lua
+++ lua/khaos/remap.lua
@@ -1,5 +1,6 @@
 vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
 vim.keymap.set("n", "<leader>s", "<cmd>bd<CR>")
+vim.keymap.set("n", "<leader>o", [[<C-w>q]])
 vim.keymap.set("n", "<leader>S", "<cmd>bd!<CR>")
 vim.keymap.set("n", "<leader>q", "<cmd>qa!<CR>")
 vim.keymap.set("n", "<leader>L", "<cmd>:Lazy<CR>")
