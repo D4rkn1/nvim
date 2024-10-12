856028cb80ab0f02cbeb71cb30e9ba7b81080792
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2024-10-12 18:05:26 +0800

    harpoon: remap and add more file shortcut

--- lua/khaos/lazy/harpoon.lua
+++ lua/khaos/lazy/harpoon.lua
@@ -7,9 +7,8 @@ return {
 
         harpoon.setup()
 
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-        vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
+        vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
+        vim.keymap.set("n", "<leader>hf", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
         vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
         vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
@@ -17,5 +16,8 @@ return {
         vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
         vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end)
         vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end)
+        vim.keymap.set("n", "<leader>8", function() harpoon:list():select(8) end)
+        vim.keymap.set("n", "<leader>9", function() harpoon:list():select(9) end)
+        vim.keymap.set("n", "<leader>0", function() harpoon:list():select(10) end)
     end,
 }
