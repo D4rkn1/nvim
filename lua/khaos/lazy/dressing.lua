5940dd70e05e36b08a1e9173f73f8d51a45e5a7c
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2024-10-13 00:16:54 +0800

    dressing: switch theme to ivy

--- lua/khaos/lazy/dressing.lua
+++ lua/khaos/lazy/dressing.lua
@@ -1,12 +1,107 @@
 return {
     "stevearc/dressing.nvim",
-    opts = {
-        input = {
-            border = "single",
-            start_in_insert = false,
-        },
-        builtin = {
-            border = "single",
-        },
-    },
+    config = function()
+        require('dressing').setup({
+            input = {
+                enabled = true,
+                default_prompt = "Input",
+                trim_prompt = true,
+                title_pos = "center",
+                start_in_insert = false,
+                border = "single",
+                relative = "editor",
+                prefer_width = 40,
+                width = nil,
+                max_width = { 140, 0.9 },
+                min_width = { 20, 0.2 },
+                buf_options = {},
+                win_options = {
+                    wrap = false,
+                    list = true,
+                    listchars = "precedes:…,extends:…",
+                    sidescrolloff = 0,
+                },
+                -- Set to `false` to disable
+                mappings = {
+                    n = {
+                        ["<Esc>"] = "Close",
+                        ["<CR>"] = "Confirm",
+                    },
+                    i = {
+                        ["<C-c>"] = "Close",
+                        ["<CR>"] = "Confirm",
+                        ["<Up>"] = "HistoryPrev",
+                        ["<Down>"] = "HistoryNext",
+                    },
+                },
+
+                override = function(conf)
+                    return conf
+                end,
+                get_config = nil,
+            },
+            select = {
+                enabled = true,
+                backend = { "telescope", "fzf_lua", "fzf", "builtin", "nui" },
+                trim_prompt = true,
+                telescope = require('telescope.themes').get_ivy({}),
+                fzf = {
+                    window = {
+                        width = 0.5,
+                        height = 0.4,
+                    },
+                },
+                fzf_lua = {
+                },
+                nui = {
+                    position = "50%",
+                    size = nil,
+                    relative = "editor",
+                    border = {
+                        style = "single",
+                    },
+                    buf_options = {
+                        swapfile = false,
+                        filetype = "DressingSelect",
+                    },
+                    win_options = {
+                        winblend = 0,
+                    },
+                    max_width = 80,
+                    max_height = 40,
+                    min_width = 40,
+                    min_height = 10,
+                },
+
+                builtin = {
+                    show_numbers = true,
+                    border = "single",
+                    relative = "editor",
+                    buf_options = {},
+                    win_options = {
+                        cursorline = true,
+                        cursorlineopt = "both",
+                    },
+                    width = nil,
+                    max_width = { 140, 0.8 },
+                    min_width = { 40, 0.2 },
+                    height = nil,
+                    max_height = 0.9,
+                    min_height = { 10, 0.2 },
+                    mappings = {
+                        ["<Esc>"] = "Close",
+                        ["<C-c>"] = "Close",
+                        ["<CR>"] = "Confirm",
+                    },
+                    telescope = require('telescope.themes').get_ivy({}),
+
+                    override = function(conf)
+                        return conf
+                    end,
+                },
+                format_item_override = {},
+                get_config = nil,
+            },
+        })
+    end,
 }
