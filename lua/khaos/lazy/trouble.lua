1d4f7d03bb5f71f7fb4ae352539cb53c304a910e
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2024-10-12 21:39:21 +0800

    trouble: fix update

--- lua/khaos/lazy/trouble.lua
+++ lua/khaos/lazy/trouble.lua
@@ -1,18 +1,22 @@
 return {
     "folke/trouble.nvim",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
+    opts = {},
+    cmd = "Trouble",
+    keys = {
+        {
+            "<leader>d<leader>",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Toggle trouble",
+        },
+        {
+            "<leader>dj",
+            "<cmd>Trouble diagnostics next jump=true<cr>",
+            desc = "Diagnostics next",
+        },
+        {
+            "<leader>dk",
+            "<cmd>Trouble diagnostics prev jump=true<cr>",
+            desc = "Diagnostics prev",
+        },
     },
-    config = function()
-        local trouble = require("trouble")
-        vim.keymap.set("n", "<leader>d<leader>", function() trouble.toggle("workspace_diagnostics") end)
-        vim.keymap.set("n", "<leader>dj", function()
-            if not trouble.is_open() then trouble.toggle("workspace_diagnostics") end
-            require("trouble").next({ skip_groups = true, jump = true })
-        end)
-        vim.keymap.set("n", "<leader>dk", function()
-            if not trouble.is_open() then trouble.toggle("workspace_diagnostics") end
-            require("trouble").previous({ skip_groups = true, jump = true })
-        end)
-    end,
 }
