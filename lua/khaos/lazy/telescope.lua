16f761e69128876a9945bbaf8193755564517f92
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2024-10-13 08:03:20 +0800

    telescope: use ivy theme in builtin functions

--- lua/khaos/lazy/telescope.lua
+++ lua/khaos/lazy/telescope.lua
@@ -13,9 +13,39 @@ return {
                     theme = "ivy",
                 },
                 live_grep = {
+                    style = {
+                        border = "single",
+                    },
                     theme = "ivy",
                 },
                 grep_strings = {
+                    style = {
+                        border = "single",
+                    },
+                    theme = "ivy",
+                },
+                git_files = {
+                    style = {
+                        border = "single",
+                    },
+                    theme = "ivy",
+                },
+                diagnostics = {
+                    style = {
+                        border = "single",
+                    },
+                    theme = "ivy",
+                },
+                buffers = {
+                    style = {
+                        border = "single",
+                    },
+                    theme = "ivy",
+                },
+                help_tags = {
+                    style = {
+                        border = "single",
+                    },
                     theme = "ivy",
                 },
             },
