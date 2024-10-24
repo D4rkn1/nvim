3bacf5b9dbb99178302578443580d7d43aef8a4f
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2024-10-24 08:10:08 +0800

    fugitive: push shortcut siltently

--- lua/khaos/lazy/fugitive.lua
+++ lua/khaos/lazy/fugitive.lua
@@ -6,7 +6,7 @@ return {
         vim.keymap.set("n", "<leader>gh", ":Telescope git_stash<CR>", { silent = true })
         vim.keymap.set("n", "<leader>gb", ":Telescope git_branches<CR>", { silent = true })
         vim.keymap.set("n", "<leader>ga", ":Git add .<CR>")
-        vim.keymap.set("n", "<leader>gp", ":Git! push<CR>")
+        vim.keymap.set("n", "<leader>gp", ":Git! push<CR>", { silent = true })
         vim.keymap.set("n", "<leader>gj", function()
             local file = vim.fn.expand("%:.")
             local gitCmd = ":Git log -p -- " .. file
