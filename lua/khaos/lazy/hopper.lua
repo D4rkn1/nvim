55b61ba1a13a25abd731e801aa1aea320443cc61
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2024-11-05 01:18:40 +0800

    vertgrid: init

--- lua/khaos/lazy/hopper.lua
+++ lua/khaos/lazy/hopper.lua
@@ -1,5 +1,6 @@
 return {
     'D4rkn1/hopper.nvim',
+    enabled = false,
     config = function()
         require('hopper').setup({
             center_window = true,
---
+++ lua/khaos/lazy/vertgrid.lua
@@ -0,0 +1,6 @@
+return {
+    'D4rkn1/vertgrid.nvim',
+    config = function ()
+        require('vertgrid').setup({size = 5})
+    end
+}
