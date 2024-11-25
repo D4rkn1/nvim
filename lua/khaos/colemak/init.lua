3d86fccacbb9d651e55703317f254b0ccd929826
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2024-11-25 19:02:21 +0800

    remap: add clipboard copy

--- lua/khaos/colemak/init.lua
+++ lua/khaos/colemak/init.lua
@@ -1,6 +1,7 @@
 vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
 vim.keymap.set("n", "<leader>rr", "<cmd>bd<CR>")
 vim.keymap.set("n", "<leader>y", [[<C-w>q]])
+vim.keymap.set("v", "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>R", "<cmd>bd!<CR>")
 vim.keymap.set("n", "<leader>q", "<cmd>qa!<CR>")
 vim.keymap.set("n", "<leader>I", "<cmd>:Lazy<CR>")
