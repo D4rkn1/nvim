dacab5cf7021e4a0feb5a8ee58712f93663c982d
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2024-12-25 17:51:07 +0800

    update plugins

--- lua/khaos/lazy/markview.lua
+++ lua/khaos/lazy/markview.lua
@@ -1,7 +1,6 @@
 return {
   "OXY2DEV/markview.nvim",
   lazy = false,
-  commit = "67b69cdaf9055bebac3682a070d7e5c8eecba29c",
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons"
