a3364e17f2eccd355f113fa3953053686f962de2
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2025-05-08 12:54:12 +0800

    lsp: remove mason old setup

--- lua/khaos/lazy/lsp.lua
+++ lua/khaos/lazy/lsp.lua
@@ -14,12 +14,7 @@ return {
       automatic_installation = true,
     }
 
-    require("mason-lspconfig").setup_handlers {
-      function(server_name)
-        require("lspconfig")[server_name].setup {
-          capabilities = capabilities
-        }
-      end,
+    require("mason-lspconfig").setup {
       ["lua_ls"] = function()
         lspconfig.lua_ls.setup {
           settings = {
