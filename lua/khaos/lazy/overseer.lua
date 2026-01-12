a2f1287a83413409b65fb6b6c3031b48b3d9d76c
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2026-01-12 17:34:28 +0800

    overseer: setup rust

--- lua/khaos/lazy/overseer.lua
+++ lua/khaos/lazy/overseer.lua
@@ -2,35 +2,83 @@ return {
   'stevearc/overseer.nvim',
   ---@module 'overseer'
   ---@type overseer.SetupOpts
-  config = function ()
-    require("overseer").setup()
+  config = function()
+    local overseer = require("overseer")
+    overseer.setup({
+      templates = {},
+    })
 
     -- meson build
-    require("overseer").register_template({
+    overseer.register_template({
       name = "meson",
       builder = function(params)
         return {
           cmd = { "meson", "setup", "build" },
-          args = {},
-          name = "Greet",
         }
       end,
-      tags = {require("overseer").TAG.BUILD},
+      tags = { overseer.TAG.BUILD },
       condition = {
         filetype = { "c", "cpp" },
       },
     })
 
-    -- ninja run
-    require("overseer").register_template({
-      name = "ninja",
-      builder = function (params)
+    -- cargo run
+    overseer.register_template({
+      name = "run",
+      builder = function(params)
+        -- This must return an overseer.TaskDefinition
         return {
-          cmd = {"ninja", "-C", "build", "run"},
-          name = "run",
+          -- cmd is the only required field. It can be a list or a string.
+          cmd = { "cargo", "run" },
+          -- additional arguments for the cmd (usually only useful if cmd is a string)
+          -- args = {},
+          -- the name of the task (defaults to the cmd of the task)
+          -- name = "Greet",
+          -- set the working directory for the task
+          -- cwd = "/tmp",
+          -- additional environment variables
+          -- env = {
+          --   VAR = "FOO",
+          -- },
+          -- the list of components or component aliases to add to the task
+          -- components = { "my_custom_component", "default" },
+          -- arbitrary table of data for your own personal use
+          -- metadata = {
+          --   foo = "bar",
+          -- },
         }
       end,
-      tags = {require("overseer").TAG.BUILD},
+      -- Optional fields
+      -- desc = "Optional description of task",
+      -- Tags can be used in overseer.run_task()
+      -- params = {
+        -- See :help overseer-params
+      -- },
+      -- Add requirements for this template. If they are not met, the template will not be visible.
+      -- All fields are optional.
+      condition = {
+        -- A string or list of strings
+        -- Only matches when current buffer is one of the listed filetypes
+        condition = {
+          callback = function()
+            return vim.bo.filetype == "rs"
+          end,
+        }
+        -- A string or list of strings
+        -- Only matches when cwd is inside one of the listed dirs
+        -- dir = "/home/user/my_project",
+      },
+    })
+
+    -- ninja run
+    overseer.register_template({
+      name = "run",
+      builder = function(params)
+        return {
+          cmd = { "ninja", "-C", "build", "run" },
+        }
+      end,
+      tags = { overseer.TAG.BUILD },
       condition = {
         filetype = { "c", "cpp" },
       },
