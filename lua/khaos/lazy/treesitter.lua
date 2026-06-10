d05d0929bbcf7b4e1fcaf20c122070e2f9faad1d
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2026-06-10 23:08:03 +0800

    treesitter: disable fold

--- lua/khaos/lazy/treesitter.lua
+++ lua/khaos/lazy/treesitter.lua
@@ -21,9 +21,13 @@ return {
         if not pcall(vim.treesitter.start, args.buf) then
           return
         end
-        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-        vim.wo.foldmethod = "expr"
+        -- vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        -- vim.wo.foldmethod = "expr"
         vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        -- vim.opt.foldlevel = 99
+        -- vim.opt.foldenable = true
+        -- vim.opt.foldtext = ""
+        -- vim.api.nvim_set_hl(0, "Folded", {})
       end,
     })
   end,
