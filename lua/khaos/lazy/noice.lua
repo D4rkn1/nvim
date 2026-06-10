afc44604f819d54a892ede87f90b95bfacaf5507
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2026-06-11 02:04:08 +0800

    formating

--- lua/khaos/lazy/noice.lua
+++ lua/khaos/lazy/noice.lua
@@ -72,7 +72,7 @@ return {
               { event = "notify" },
               { error = true },
               { warning = true },
-              { event = "msg_show"},
+              { event = "msg_show" },
               { event = "lsp", kind = "message" },
             },
           },
